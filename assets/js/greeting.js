cdcddb6ced777f6f50d676e5c9bbf7b20e07ad9f
Sujeevan <sujeevanbalamany@gmail.com>
2024-07-19 12:41:50 +0530

    feat: developed greeting card with dynamic name

--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -1,7 +1,19 @@
 import {Fireworks} from "fireworks-js"
 
 const fwContainer = document.querySelector("#fireworks")
+const greeterNameEl = document.querySelector("#greeterName")
 
 const fireworks = new Fireworks(fwContainer);
 
 fireworks.start()
+
+
+    const url = new URL(window.location.href);
+    const getGreeterName = url.searchParams.get("name");
+
+    if(getGreeterName){
+        greeterNameEl.textContent = getGreeterName;
+    }else{
+        location.href = "/festival-greeting-app/index.html"
+    }
+    
