4af97d3073e501f1aba5502789b2aade97725959
Sujeevan <sujeevanbalamany@gmail.com>
2024-07-19 10:09:09 +0530

    design: greeting page with fireworks.js

---
+++ assets/js/greeting.js
@@ -0,0 +1,7 @@
+import {Fireworks} from "fireworks-js"
+
+const fwContainer = document.querySelector("#fireworks")
+
+const fireworks = new Fireworks(fwContainer);
+
+fireworks.start()
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base : '/festival-greeting-app/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        greetingPage: resolve(__dirname, 'greeting.html'),
+      },
+    },
+  },
 })
+
